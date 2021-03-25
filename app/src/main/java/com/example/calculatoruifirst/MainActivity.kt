b151086f41b6c9f549216b1f958e525892c08a96
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2021-03-25 20:45:22 +0300

    lastbut not least

--- app/src/main/java/com/example/calculatoruifirst/MainActivity.kt
+++ app/src/main/java/com/example/calculatoruifirst/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.calculatoruifirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val birinci
+        val birinciSayi:EditText=findViewById(R.id.firstNumber)
+        val ikinciSayi:EditText=findViewById(R.id.ikinciSayi)
+        val sonuc:TextView=findViewById(R.id.solution)
+        val toplama:Button=findViewById(R.id.addition)
+
+        toplama.setOnClickListener {
+            val number1:Int=Integer.parseInt(birinciSayi.toString())
+            val number2:Int=Integer.parseInt((ikinciSayi.toString()))
+            val last=number1+number2
+            sonuc.text=last.toString()
+        }
+
+
+        val cikarma:Button=findViewById(R.id.substraction)
+        val carpma:Button=findViewById(R.id.multiplication)
+        var bolme:Button=findViewById(R.id.division)
     }
 }
